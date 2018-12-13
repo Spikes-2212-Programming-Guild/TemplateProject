8d9afabf10fca6ebcc093ccf70ca4f491e97f339
uriahrokach <uriahrokach@gmail.com>
2018-12-13 20:29:08 +0200

    Added Joysticks and controller

--- TemplateProject/src/org/usfirst/frc/team2212/robot/OI.java
+++ TemplateProject/src/org/usfirst/frc/team2212/robot/OI.java
@@ -7,10 +7,19 @@
 
 package org.usfirst.frc.team2212.robot;
 
+import com.spikes2212.utils.XboXUID;
+
+import edu.wpi.first.wpilibj.Joystick;
+
 public class OI /*GEVALD*/ {
 	
+	private Joystick driverLeft = new Joystick(0);
+	private Joystick driverRight = new Joystick(1);
+
+	private XboXUID navigator = new XboXUID(2);
 	
 	public OI(){
 		
 	}
+
 }
